20ed9ce4cf393bda80887b6134adee6bb26bdabc
STE1Nhb <bespalivoo@gmail.com>
2023-03-05 02:06:22 +0300

    RankedGradeBook's "GetLetterGrade" method overriden.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,33 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int students = Students.Count;
+            List<double> avgGrades = new List<double>();
+
+            foreach (var student in Students) 
+            {
+                avgGrades.Add(student.AverageGrade);
+            }
+
+            int grades = Convert.ToInt32(avgGrades.Count * 0.2);
+            avgGrades.Sort(); avgGrades.Reverse();
+            double[] avgGradesArray = avgGrades.ToArray();
+
+            if (students < 5)
+                throw new InvalidOperationException();
+            else if (averageGrade >= avgGradesArray [grades - 1] && averageGrade <= avgGradesArray[0])
+                return 'A';
+            else if (averageGrade >= avgGradesArray [2 * grades - 1] && averageGrade <= avgGradesArray[grades])
+                return 'B';
+            else if (averageGrade >= avgGradesArray [3 * grades - 1] && averageGrade <= avgGradesArray[2 * grades])
+                return 'C';
+            else if (averageGrade >= avgGradesArray [4 * grades - 1] && averageGrade <= avgGradesArray[3 * grades])
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
